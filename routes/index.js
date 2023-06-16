5bdad5bb3b8f259fc3c4f2278e3e1f96156716fe
nolanm99 <npm65@drexel.edu>
2023-06-16 10:41:38 -0400

    Add title and app version in footer, update config file

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
-var express = require('express');
-var router = express.Router();
+const express   = require('express');
+const config    = require('config');
+const router    = express.Router();
+const DB_CONFIG = config.DB_CONFIG;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'List.me' });
 });
 
 module.exports = router;
--- routes/list.js
+++ routes/list.js
@@ -1,17 +1,20 @@
 const express               = require('express');
 const config                = require('config');
 const ShoppingListDatabase  = require('../helper/shopping_list_db');
-const router = express.Router();
-const DB_CONFIG = config.DB_CONFIG;
+const router                = express.Router();
 
-db_url = process.env.DB_URL || DB_CONFIG.URL;
-db_name = process.env.DB_NAME || DB_CONFIG.NAME;
+
+db_url = process.env.DB_URL || config.URL;
+db_name = process.env.DB_NAME || config.NAME;
 shopping_list_db = new ShoppingListDatabase(db_url, db_name);
 
 router.get('/', async (req, res, next) => {
     all_lists = await shopping_list_db.get_all_lists();
     console.log(all_lists);
-    res.render('index', { title: 'Express' });
+    res.render('index', {
+        title: 'Express',
+        app_version: config.APP_VERSION
+    });
 });
 
 router.get('/new_list', async (req, res, next) => {
@@ -24,6 +27,8 @@ router.get('/:list_id', async (req, res, next) => {
     var list_id = req.params.list_id
     var list_contents = await shopping_list_db.get_list_contents(list_id);
     res.render('list', {
+        title: "List View",
+        app_version: config.APP_VERSION,
         items: Object.values(list_contents),
         list_id: list_id
     });
