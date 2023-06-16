0adaaa40de174b2568df4260a5c027328d703f77
nolanm99 <npm65@drexel.edu>
2023-06-16 00:56:24 -0400

    Make header and footer template, use copy icon

---
+++ public/javascripts/list.js
@@ -0,0 +1,72 @@
+async function copyToClipboard() {
+    var copyText = document.getElementById("list_id");
+    try {
+        await navigator.clipboard.writeText(copyText.innerText);
+        alert('Text copied to clipboard');
+    } catch (err) {
+        alert('Failed to copy text');
+    }
+}
+
+async function saveChanges() {
+var new_list_html = Array.from(document.getElementById('shopping_list_items').children);
+var new_list_text = [];
+new_list_html.forEach( (item_html)  => {
+    if (item_html.innerText) {
+    new_list_text.push(item_html.innerText);
+    }
+})
+console.log(`saving new list`);
+console.log(new_list_text)
+
+list_id = document.getElementById("list_id").innerText;
+
+await fetch('/list/update_list', {
+    method: 'POST',
+    headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ "list_id": list_id, "new_list_contents": new_list_text })
+})
+}
+
+window.onload = function() {
+var list = document.getElementById('shopping_list_items');
+for (var i = 0; i < list.children.length; i++) {
+    addEnterListener(list.children[i]);
+}
+}
+
+function addEnterListener(element) {
+element.addEventListener('keydown', function(e) {
+    if (e.key === 'Enter') {
+        e.preventDefault();
+        var nextElement = getNextElement(element);
+        if (nextElement) {
+            nextElement.focus();
+        } else {
+            addListItem();
+        }
+    }
+});
+}
+
+function addListItem() {
+    var list = document.getElementById('shopping_list_items');
+    var newListElement = document.createElement('li');
+    newListElement.contentEditable = 'true';
+    newListElement.innerText = '';
+    newListElement.classList.add('list-group-item');
+    addEnterListener(newListElement);
+    list.appendChild(newListElement);
+    newListElement.onblur = saveChanges;
+    newListElement.focus();
+}
+
+function getNextElement(element) {
+    do {
+        element = element.nextSibling;
+    } while (element && element.nodeType !== 1);
+    return element;
+}
