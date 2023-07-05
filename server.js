7f96616f56481e6e9cd8faea4b5097e42f39fd19
nolanm99 <nolanm99@gmail.com>
2023-07-04 22:01:32 -0400

    improve home page

--- server.js
+++ server.js
@@ -47,6 +47,6 @@ app.use(function(err, req, res, next) {
 });
 
 const port = process.env.PORT || 3000;
-app.listen(port, () =>
+app.listen(port, '0.0.0.0', () =>
   console.log(`Example app is listening on port ${port}.`)
 );
